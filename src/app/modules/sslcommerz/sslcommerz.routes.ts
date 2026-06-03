a0a87c0c1020f5f1d0e4462f76fce4b77d9a9cae
Shaun Hossain <shaun.mononsoft@gmail.com>
2026-06-03 20:11:38 +0600

    env variable updated

--- src/app/modules/sslcommerz/sslcommerz.routes.ts
+++ src/app/modules/sslcommerz/sslcommerz.routes.ts
@@ -5,9 +5,6 @@ const router = Router();
 
 // Define routes
 
-router.post(
-    '/validate',
-    SSLController.validatePaymentService
-)
+router.post('/validate', SSLController.validatePaymentService);
 
 export const SSLRoutes = router;
